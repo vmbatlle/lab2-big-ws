9ed38f1a96e3d533fb6470018e7535e601524152
Victor M. Batlle <736478@unizar.es>
2019-10-31 19:01:19 +0100

    Add unimplemented attributes in Translator Exception class

--- src/main/java/translator/exception/TranslatorException.java
+++ src/main/java/translator/exception/TranslatorException.java
@@ -1,6 +1,9 @@
 package translator.exception;
 
 public class TranslatorException extends RuntimeException {
+
+  private static final long serialVersionUID = 7094604538630942965L;
+
   public TranslatorException(String message) {
     super(message);
   }
